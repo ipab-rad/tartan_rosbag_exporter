68c4cd7b7c7ecf740633b34319099b3fbef5f833
Hector Cruz <hcruzgo@ed.ac.uk>
2025-06-26 11:36:27 +0000

    WIP: Add support for Hesai LidarPoints

--- ros2_bag_exporter/include/rosbag2_exporter/handlers/pointcloud_handler.hpp
+++ ros2_bag_exporter/include/rosbag2_exporter/handlers/pointcloud_handler.hpp
@@ -25,6 +25,26 @@
 namespace rosbag2_exporter
 {
 
+/**
+ * This point type is not using PCL_ADD_POINT4D to avoid the addition of a 32-bit dummy word.
+ * The fields are ordered to meet the SSE alignment.
+*/
+struct PointXYZIRCAEDT
+{
+  float x;
+  float y;
+  float z;
+  std::uint8_t intensity;
+  std::uint8_t return_type;
+  std::uint16_t channel;
+  float azimuth;
+  float elevation;
+  float distance;
+  std::uint32_t time_stamp;
+};
+
+using DefaultPointType = PointXYZIRCAEDT;
+// using DefaultPointCloud = pcl::PointXYZI;
 class PointCloudHandler : public BaseHandler
 {
 public:
@@ -74,9 +94,9 @@ public:
 
     // Create the point cloud, convert the ROS message, and save it
     if (has_intensity) {
-      pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+      pcl::PointCloud<DefaultPointType>::Ptr cloud(new pcl::PointCloud<DefaultPointType>);
       pcl::fromROSMsg(pc2_msg, *cloud);
-      return save_pointcloud_to_file<pcl::PointXYZI>(cloud, data_meta.data_path);
+      return save_pointcloud_to_file<DefaultPointType>(cloud, data_meta.data_path);
     } else {
       throw std::invalid_argument("The pointcloud message should have an 'intensity' field!");
     }
@@ -104,4 +124,10 @@ private:
 
 }  // namespace rosbag2_exporter
 
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+  rosbag2_exporter::PointXYZIRCAEDT,
+  (float, x, x)(float, y, y)(float, z, z)(std::uint8_t, intensity, intensity)(
+    std::uint8_t, return_type,
+    return_type)(std::uint16_t, channel, channel)(float, azimuth, azimuth)(
+    float, elevation, elevation)(float, distance, distance)(std::uint32_t, time_stamp, time_stamp))
 #endif  // ROSBAG2_EXPORTER__HANDLERS__POINTCLOUD_HANDLER_HPP_
